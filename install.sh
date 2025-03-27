4308798803ef9cfcb472ce5a46f491e113b659c1
datsrt02 <63861231+datsrt02@users.noreply.github.com>
2025-03-27 15:23:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo chown -R 1000:1000 n8n
 sudo chmod -R 755 n8n
 echo "--------- 🔴 Finish creating folder -----------"
 echo "--------- 🟢 Start docker compose up  -----------"
-wget https://raw.githubusercontent.com/datsrt02/n8n/refs/heads/main/compose_noai.yaml -O compose.yaml
+wget https://raw.githubusercontent.com/datsrt02/n8n_docker_proxmox/refs/heads/main/compose.yaml?token=GHSAT0AAAAAAC7KLGKPNQQB3CVF6BOMKTGOZ7FBM7Q -O compose.yaml
 export EXTERNAL_IP=http://"$(hostname -I | cut -f1 -d' ')"
 sudo -E docker compose up -d
 echo "--------- 🔴 Finish! Wait a few minutes and test in browser at url $EXTERNAL_IP for n8n UI -----------"
